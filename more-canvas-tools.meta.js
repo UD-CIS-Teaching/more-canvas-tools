0c4330f1481df82070e6cb126c6b8a0e1cc3ed02
Austin Cory Bart <acbart@vt.edu>
2022-07-22 15:32:11 -0400

    Updates

--- more-canvas-tools.meta.js
+++ more-canvas-tools.meta.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name         More Canvas Tools
-// @version      0.1.0.2022.7.22.15.17
+// @version      0.1.0.2022.7.22.15.31
 // @description  Next generation canvas tools for simplifying your life
 // @author       Austin Cory Bart
 // @match        *://*.instructure.com/*
 // @namespace    udcis.canvas
 // @run-at       document-start
 // @grant        none
-// @updateURL    https://github.com/UD-CIS-Teaching/more-canvas-tools/raw/master/dist/more-canvas-tools.user.js
-// @downloadURL  https://github.com/UD-CIS-Teaching/more-canvas-tools/raw/master/dist/more-canvas-tools.user.js
+// @updateURL    https://ud-cis-teaching.github.io/more-canvas-tools/more-canvas-tools.user.js
+// @downloadURL  https://ud-cis-teaching.github.io/more-canvas-tools/more-canvas-tools.user.js
 // ==/UserScript==
